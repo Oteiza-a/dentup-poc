5f52c36e65035021ae6dcf56019f427eb8e59a61
oteiza-a <alberto.jesus.oteiza@gmail.com>
2023-03-07 10:04:09 -0300

    testing nextjs dynamic import on patient content

--- pages/patients/[id].tsx
+++ pages/patients/[id].tsx
@@ -1,50 +1,21 @@
-import Layout from '@/components/layout/Layout';
-import { Card, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
 import React from 'react';
-import { v4 as uuid } from 'uuid';
-import Header from '@/components/header/Header';
 import { GetServerSideProps } from 'next';
 import { getPatient } from '../api/patients/[patientId]';
 import { IPatient } from '@/interfaces/IPatient';
 import { toCleanObject } from '@/utils/object';
-import { PatientForm } from '@/components/patient-form/PatientForm';
+import dynamic from 'next/dynamic';
 
 interface Props extends React.PropsWithChildren {
   isNewPatient: boolean
   patient?: IPatient,
 }
 
-const Content = ({ isNewPatient, patient }: any) => {
-  const tabs: { key: string, text: string, component: JSX.Element }[] = [
-    { key: uuid(), text: 'Información de paciente', component: <PatientForm patientData={patient} isNewPatient={isNewPatient} /> },
-    { key: uuid(), text: 'Archivos', component: <>Archivos!</> },
-    { key: uuid(), text: 'Evolución', component: <>Evolución!</> },
-  ]
-
-  return (
-    <Layout navbar>
-      <Header backButtonText='Volver' />
-      <Card p='4'>
-        <Tabs variant='enclosed-colored' colorScheme='blue'>
-
-          <TabList>
-            {tabs.map(({ text }) => <Tab key={text} style={{ borderRadius: '6px 6px 0px 0px' }}>{text}</Tab>)}
-          </TabList>
-
-          <TabPanels>
-            {tabs.map(({ key, component }) => <TabPanel key={key}>{component}</TabPanel>)}
-          </TabPanels>
-
-        </Tabs>
-      </Card>
-    </Layout>
-  )
-}
+const PatientContent = dynamic(() => import('@/components/patient-content/PatientContent'));
 
 const Patient: React.FC<Props> = ({ isNewPatient, patient }) => {
 
   return (
-    <Content isNewPatient={isNewPatient} patient={patient}/>
+    <PatientContent isNewPatient={isNewPatient} patient={patient}/>
   );
 };
 
@@ -59,7 +30,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     const patient = await getPatient(id as string);
     props.patient = toCleanObject(patient);
   }
-  
+
   return { props }
 }
 
